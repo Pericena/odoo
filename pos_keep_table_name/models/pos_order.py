1219d8c0aa17df513c34ad8b6463ff763639e551
Luishiño <pericena12@gmail.com>
2025-07-25 19:26:49 -0400

    init2

--- pos_keep_table_name/models/pos_order.py
+++ pos_keep_table_name/models/pos_order.py
@@ -19,31 +19,3 @@ class PosOrder(models.Model):
             order['table'] = order.get('table', '')
             order['customer_name'] = order.get('customer_name', '')
         return res
-
-    @api.model
-    def create_from_ui(self, orders, draft=False):
-        created_order_ids = []
-        for ui_order in orders:
-            table_name = ui_order.get('table', '')
-            customer_name = ui_order.get('customer_name', '')
-
-            existing_order = self.search([
-                ('table', '=', table_name),
-                ('state', 'in', ['draft', 'sent']),
-            ], limit=1)
-
-            if existing_order:
-                for line in ui_order.get('lines', []):
-                    self.env['pos.order.line'].create({
-                        'order_id': existing_order.id,
-                        'product_id': line[2]['product_id'],
-                        'qty': line[2]['qty'],
-                        'price_unit': line[2]['price_unit'],
-                        'name': line[2].get('name', ''),
-                        'note': line[2].get('note', ''),
-                    })
-                created_order_ids.append(existing_order.id)
-            else:
-                ids = super().create_from_ui([ui_order], draft)
-                created_order_ids.extend(ids)
-        return created_order_ids
--- pos_keep_table_name/static/src/js/keep_table_name.js
+++ pos_keep_table_name/static/src/js/keep_table_name.js
@@ -2,34 +2,26 @@ odoo.define('pos_keep_table_name.KeepTableName', function (require) {
     'use strict';
 
     const Order = require('point_of_sale.models').Order;
-    const { patch } = require("@web/core/utils/patch");
 
-    patch(Order.prototype, {
-        initialize() {
-            this._super(...arguments);
-            this.saved_customer_name = this.customer_name || this.table?.name || '';
-            this.saved_table_name = this.table?.name || '';
-        },
+    const super_init = Order.prototype.initialize;
+    Order.prototype.initialize = function (attributes, options) {
+        super_init.call(this, attributes, options);
+        this.saved_customer_name = this.table?.name || this.customer_name || "";
+        this.saved_table_name = this.table?.name || "";
+    };
 
-        export_as_JSON() {
-            const json = this._super(...arguments);
-            json.customer_name = this.saved_customer_name;
-            json.table = this.saved_table_name;
-            return json;
-        },
+    const super_export_as_JSON = Order.prototype.export_as_JSON;
+    Order.prototype.export_as_JSON = function () {
+        const json = super_export_as_JSON.call(this);
+        json.customer_name = this.saved_customer_name;
+        json.table = this.saved_table_name;
+        return json;
+    };
 
-        init_from_JSON(json) {
-            this._super(...arguments);
-            this.saved_customer_name = json.customer_name || '';
-            this.saved_table_name = json.table || '';
-            this.customer_name = this.saved_customer_name;
-
-            if (this.pos.tables && this.saved_table_name) {
-                const table = this.pos.tables.find(t => t.name === this.saved_table_name);
-                if (table) {
-                    this.table = table;
-                }
-            }
-        },
-    });
+    const super_init_from_JSON = Order.prototype.init_from_JSON;
+    Order.prototype.init_from_JSON = function (json) {
+        super_init_from_JSON.call(this, json);
+        this.saved_customer_name = json.customer_name || "";
+        this.saved_table_name = json.table || "";
+    };
 });
