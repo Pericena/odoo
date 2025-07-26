6cc622cec3e18d8d23cfcfba178bfe2776db92f4
Luishiño <pericena12@gmail.com>
2025-07-26 01:08:26 -0400

    test3

--- pos_keep_table_name/models/pos_order.py
+++ pos_keep_table_name/models/pos_order.py
@@ -1,27 +1,23 @@
-from odoo import models, fields
+from odoo import models, api
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class PosOrder(models.Model):
     _inherit = 'pos.order'
-
-    table = fields.Char(string="Mesa")  
-    customer_name = fields.Char(string="Cliente")
-    original_table_name = fields.Char(string="Nombre Original", copy=False)
-
+    
     @api.model
-    def _order_fields(self, ui_order):
-        res = super()._order_fields(ui_order)
-        table_name = str(ui_order.get('table', '')) if ui_order.get('table') else ''
-        
-        res['table'] = table_name
-        res['original_table_name'] = table_name
-        res['name'] = table_name 
-        
-        res['customer_name'] = str(ui_order.get('customer_name', '')) if ui_order.get('customer_name') else ''
-        return res
+    def create(self, vals):
+        _logger.info("CREATING PosOrder with values: %s", vals)
+        return super(PosOrder, self).create(vals)
+
+    def write(self, vals):
+        _logger.info("UPDATING PosOrder ID %s with values: %s", self.ids, vals)
+        return super(PosOrder, self).write(vals)
 
     def _get_pos_ui_data(self):
         res = super()._get_pos_ui_data()
         for order in res.get('orders', []):
-            if isinstance(order, dict):
-                order['original_table_name'] = order.get('table', '') 
+            order['table'] = order.get('table', '')
+            order['customer_name'] = order.get('customer_name', '')
         return res
